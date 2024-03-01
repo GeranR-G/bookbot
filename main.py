521751df8e90ac9520457af420b4f43422ec52eb
GeranR-G <rustgreeng@gmail.com>
2024-03-01 13:11:48 -0700

    moved the list sorting from the report function to the convert to list function

--- main.py
+++ main.py
@@ -24,18 +24,19 @@ def sort_on(dict):
 def report(text):
     num_words = get_num_words(text)
     output = f"--- Begin report of books/frankenstein.txt ---\n{num_words} words found in the document\n"
-    char_list = convert_to_alpha_list(find_char_amount(text))
-    char_list.sort(reverse=True, key=sort_on)
+    char_list = convert_to_sorted_list(find_char_amount(text))
     for i in char_list:
         output += f"\nThe '{i["char"]}' character was found {i["num"]} times"
     output += "\n--- End report ---"
     return output
 
-def convert_to_alpha_list(dict):
+def convert_to_sorted_list(dict):
     alpha_list = []
     for i in dict:
         if i.isalpha():
             alpha_list.append({"char": i, "num": dict[i]})
+    
+    alpha_list.sort(reverse=True, key=sort_on)
     return alpha_list
 
 def get_book_text(path_to_file):
